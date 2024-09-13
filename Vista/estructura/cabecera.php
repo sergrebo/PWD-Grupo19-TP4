eb3c756b0e8d84cf8254d2d5cf92a42acea8a927
Nicolas Matias Antinao <nicoantinao1998@gmail.com>
2024-09-13 20:27:49 -0300

    Arregla estilos

--- Vista/estructura/cabecera.php
+++ Vista/estructura/cabecera.php
@@ -14,7 +14,13 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.21.0/jquery.validate.min.js"></script>
   <!-- Bootstrap CDN via jsDelivr -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/estilos.css">
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+  </style>
   
 </head>
 
