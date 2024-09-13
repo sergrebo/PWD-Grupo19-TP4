db02a68a197ef1b48f9ec68318fef56287e09d78
sergrebo <sergio.rebolledo@est.fi.uncoma.edu.ar>
2024-09-13 17:39:59 -0300

    Creación de index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Bootstrap CDN via jsDelivr -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <div class="row col-4 m-auto">
+    <div class="w-100">
+      <h1>Elija su opción</h1>
+      <div class="d-grid gap-2">
+        <button type="button" class="btn btn-primary w-100">Button</button>
+        <button type="button" class="btn btn-primary w-100">Button</button>
+        <button type="button" class="btn btn-primary w-100">Button</button>
+        <button type="button" class="btn btn-primary w-100">Button</button>
+      </div>
+
+    </div>
+  </div>
+
+</body>
+
+</html>
